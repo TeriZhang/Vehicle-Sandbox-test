8c5e2d55815c14a006ffef94c3e2a38d35aca026
ShaotingWang <149331919+ShaotingWang@users.noreply.github.com>
2025-11-29 13:21:39 -0600

    incomplete building system progress
    Placing Objects, Attachment points.

--- Assets/Global.cs
+++ Assets/Global.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
 using UnityEngine.InputSystem.iOS;
+using UnityEngine.UI;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
@@ -18,7 +19,17 @@ public class Global : MonoBehaviour
     public Rigidbody2D rb;
     public List<GameObject> listOfParts;
 
-    public string gameState;
+    string gameState;
+
+    string userActionState = "Idle"; // Build, Delete, Idle
+
+    public string SelectedPart;
+
+    public GameObject ghostPart;
+
+    public GameObject BuildingUI;
+
+    public GameObject BlockButton;
 
     void freeze(GameObject part, bool f)
     {
@@ -30,36 +41,83 @@ public class Global : MonoBehaviour
 
     GameObject newRoot()
     {
-        GameObject temp = Instantiate(Resources.Load<GameObject>("basicPart"), transform);
+        GameObject temp = Instantiate(Resources.Load<GameObject>("Parts/Block"), transform);
         temp.GetComponent<SpriteRenderer>().color = Color.pink;
         temp.name = "Root";
         temp.transform.position = new Vector2(0, 2);
-        freeze(temp, true);
         return temp;
     }
     public GameObject newPart(string name, GameObject weldParent, Vector2 anchor)
     {
-        GameObject temp = Instantiate(Resources.Load<GameObject>("basicPart"), transform);
-        temp.name = "basicPart";
+        GameObject temp = Instantiate(Resources.Load<GameObject>("Parts/Block"), transform);
+        temp.name = "Block";
+        Destroy(temp.GetComponent<Rigidbody2D>());
         temp.transform.position = (Vector2)weldParent.transform.position + anchor;
         temp.transform.parent = weldParent.transform;
 
         listOfParts.Add(temp);
 
-        freeze(temp, true); // temporary
         return temp;
     }
     void CreateVehicle()
     {
         Root = newRoot();
 
-        newPart("basicPart", Root, new Vector2(-1, 0));
-        newPart("basicPart", Root, new Vector2(0, 1));
-        topPart = newPart("basicPart", Root, new Vector2(1, 0));
+        newPart("Block", Root, new Vector2(-1, 0));
+        newPart("Block", Root, new Vector2(0, 1));
+        topPart = newPart("Block", Root, new Vector2(1, 0));
 
-        GameObject topBPart = newPart("basicPart", topPart, new Vector2(1, 0));
-        newPart("basicPart", topBPart, new Vector2(1, 0));
-        newPart("basicPart", topBPart, new Vector2(0, 1));
+        GameObject topBPart = newPart("Block", topPart, new Vector2(1, 0));
+        newPart("Block", topBPart, new Vector2(1, 0));
+        newPart("Block", topBPart, new Vector2(0, 1));
+    }
+
+    void snapToAttachPoint()
+    {
+                //select an attachment point on the body with the mouse.
+        Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        double distance;
+        double closestDist = int.MaxValue;
+        Vector2 closestAttachPoint = new Vector2();
+        GameObject closestPart;
+
+        foreach (GameObject Part in listOfParts)
+        {
+            foreach (Vector2 AttachPoint in Part.GetComponent<properties>().AttachPoints)
+            {
+                distance = (mousePos - AttachPoint).magnitude;
+                if (distance < 3 && distance < closestDist)
+                {
+                    closestDist = distance;
+                    closestAttachPoint = AttachPoint;
+                    closestPart = Part;
+                }
+            }
+        }
+        if (closestAttachPoint != new Vector2())
+        {
+            Debug.Log(closestAttachPoint);
+        }
+        else
+        {
+            Debug.Log("none");
+        }
+    }
+    
+    void SelectPart(string PartName)
+    {
+        userActionState = 
+        SelectedPart = PartName;
+        ghostPart = Resources.Load<GameObject>("Parts/" + PartName);
+    }
+
+    void onLeftMouseDown()
+    {
+        
+    }
+    void onLeftMouseUp()
+    {
+        
     }
 
     void Start()
@@ -71,27 +129,10 @@ public class Global : MonoBehaviour
 
         //Building system. Build parts onto attachment points, delete parts, adjusting parts.
 
+        BlockButton.GetComponent<Button>().onClick.AddListener(() => SelectPart("Block"));
 
-        //select an attachment point on the body with the mouse.
-        Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        double distance;
-        double closestDist = int.MaxValue;
-        Vector2 closestAttachPoint;
 
-        Vector2 AttachPoint = new Vector2(0, 1); // temporary
-        foreach (GameObject Part in listOfParts)
-        {
-            // foreach (Vector2 AttachPoint in Part)
-            // {
-            // }
-            distance = (mousePos - AttachPoint).magnitude;
-            if (distance < 3 && distance < closestDist)
-            {
-                closestDist = distance;
-                closestAttachPoint = AttachPoint;
-            }
-        }
-
+        
     }
     void Update()
     {
---
+++ Assets/properties.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class properties : MonoBehaviour
+{
+    public List<Vector3> AttachPoints = new List<Vector3>();
+}
